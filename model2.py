9601c0d97d6a2be5ecace099079c462227cbab1a
kwoncy <beefbreaker2020@gmail.com>
2022-10-06 19:54:34 +0900

    add MobileDense model

--- model2.py
+++ model2.py
@@ -129,38 +129,109 @@ class Res1(tf.keras.layers.Layer):
         return x10
 
 
-input_layer = Input(shape=(64000,1), batch_size=BATCH_SIZE)
+class MobileDense1(tf.keras.layers.Layer):
+    ## the number of filters must be twice of input channels because the output will be concatenated. 
+    def __init__(self,filters:int, kernel_size:int=3, padding:str='same', activation:str='relu', depth_mul:int=4):
+        super(MobileDense1,self).__init__()
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.activation = activation
+        self.depth_mul = depth_mul
 
-res1 = Res1(filters, 3, 'same', ACTIVATION_FN)(input_layer)
-## 32000, 32
+        if self.filters & 1:
+            raise Exception("from MobileDense1: the parameter named 'filters' must be even number")
 
-res2 = Res1(filters*2, 3, 'same', ACTIVATION_FN)(res1)
-## 16000, 64
+        self.conv1 = DepthwiseConv2D(self.kernel_size, depth_multiplier=self.depth_mul, padding=self.padding)
+        self.bn1 = BatchNormalization()
+        self.act1 = Activation(self.activation)
+        self.conv2 = Conv2D(self.filters//2, 1, padding=self.padding)
+        self.bn2 = BatchNormalization()
+        self.act2 = Activation(self.activation)
+        self.pool = MaxPool2D(strides=2)
+        
+    def __call__(self, input):
+        if input.shape[-1] != self.filters//2:
+            raise Exception("from MobileDense1: the input channels must be half of the 'filters' parameter")
 
-res3 = Res1(filters*4, 3, 'same', ACTIVATION_FN)(res2)
-## 8000, 128
+        x1 = self.conv1(input)
+        x2 = self.bn1(x1)
+        x3 = self.act1(x2)
+        x4 = self.conv2(x3)
+        x5 = self.bn2(x4)
+        x6 = self.act2(x5)
+        x7 = tf.concat([input, x6],axis=-1)
+        
+        return self.pool(x7)
 
-res4 = Res1(filters*8, 3, 'same', ACTIVATION_FN)(res3)
-## 4000, 256
 
-res5 = Res1(filters*16, 3, 'same', ACTIVATION_FN)(res4)
-## 2000, 512
+if "__main__" == __name__:
 
-conv1 = Conv1D(filters *16, kernel_size=3, padding='same')(res5)
-## 2000, 512
-tr1 = tf.transpose(conv1,perm=[0,2,1])
-# print("input_layer, ", res10)
-enc1 = MyEncoder(2000,8,BATCH_SIZE)(tr1)
 
-flat1 = Flatten()(enc1)
-batch6 = BatchNormalization()(flat1)
-dense1 = Dense(200, activation='relu')(batch6)
-batch7 = BatchNormalization()(dense1)
-dense2 = Dense(20, activation='relu')(batch7)
-# dense2 = Dense(20, activation='relu')(dense1)
-output_layer = Dense(1, activation='sigmoid')(dense2)
+    # input_layer = Input(shape=(64000,1), batch_size=BATCH_SIZE)
 
-model = Model(input_layer, output_layer)
+    # res1 = Res1(filters, 3, 'same', ACTIVATION_FN)(input_layer)
+    # ## 32000, 32
 
-# model.summary()
+    # res2 = Res1(filters*2, 3, 'same', ACTIVATION_FN)(res1)
+    # ## 16000, 64
 
+    # res3 = Res1(filters*4, 3, 'same', ACTIVATION_FN)(res2)
+    # ## 8000, 128
+
+    # res4 = Res1(filters*8, 3, 'same', ACTIVATION_FN)(res3)
+    # ## 4000, 256
+
+    # res5 = Res1(filters*16, 3, 'same', ACTIVATION_FN)(res4)
+    # ## 2000, 512
+
+    # conv1 = Conv1D(filters *16, kernel_size=3, padding='same')(res5)
+    # ## 2000, 512
+    # tr1 = tf.transpose(conv1,perm=[0,2,1])
+    # # print("input_layer, ", res10)
+    # enc1 = MyEncoder(2000,8,BATCH_SIZE)(tr1)
+
+    # flat1 = Flatten()(enc1)
+    # batch6 = BatchNormalization()(flat1)
+    # dense1 = Dense(200, activation='relu')(batch6)
+    # batch7 = BatchNormalization()(dense1)
+    # dense2 = Dense(20, activation='relu')(batch7)
+    # # dense2 = Dense(20, activation='relu')(dense1)
+    # output_layer = Dense(1, activation='sigmoid')(dense2)
+
+    # model = Model(input_layer, output_layer)
+
+    # model.summary()
+
+    
+    input_layer = Input(shape=(64000,1), batch_size=BATCH_SIZE)
+    conv = Conv1D(320,3, padding='same')(input_layer)
+    bn = BatchNormalization()(conv)
+    act = Activation('relu')(bn)
+    reshaped1 = tf.reshape(act, (-1,800,800,32))
+    md1 = MobileDense1(64)(reshaped1)
+    ## 400, 64
+
+    md2 = MobileDense1(128)(md1)
+    ## 200, 128
+
+    md3 = MobileDense1(256)(md2)
+    ## 100, 256
+
+    md4 = MobileDense1(512)(md3)
+    ## 50, 512
+
+    reshaped2 = tf.reshape(md4,(-1,2500,512))
+    enc1 = MyEncoder(2500,10,BATCH_SIZE)(reshaped2)
+
+    flat1 = Flatten()(enc1)
+    batch6 = BatchNormalization()(flat1)
+    dense1 = Dense(200, activation='relu')(batch6)
+    batch7 = BatchNormalization()(dense1)
+    dense2 = Dense(20, activation='relu')(batch7)
+    # dense2 = Dense(20, activation='relu')(dense1)
+    output_layer = Dense(1, activation='sigmoid')(dense2)
+
+    model = Model(input_layer, output_layer)
+
+    model.summary()
